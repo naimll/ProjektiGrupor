111827ac60fea9b38805812d4d137f334aabbd46
naimll <nl45574@ubt-uni.net>
2020-06-11 22:28:53 +0200

    11/06/2020 commit(1)

--- LibraryManagmentSystem.sql
+++ LibraryManagmentSystem.sql
@@ -57,18 +57,10 @@ foreign key(L_Kategoria_Id) references Kategoria_Librit(Kategoria_Id)
 create table Huazimi_Librit(
 HL_Id int primary key,
 HL_Data_Leshimit date not null,
-HL_Afati_Kthimit date not null,
+HL_Data_Kthimit date not null,
 HL_Klienti_Id int,
 HL_ISBN varchar(100),
 foreign key(HL_Klienti_Id) references Klienti(K_Id),
 foreign key(HL_ISBN) references Libri(ISBN)
 )
-create table Kthimi_Librit(
-KthL_Id int primary key,
-KthL_Data_Leshuar date,
-KthL_Data_Kthyer date,
-KthL_Klienti_Id int,
-KthL_ISBN varchar(100)
-foreign key(KthL_Klienti_Id) references Klienti(K_ID),
-foreign key(KthL_ISBN) references Libri(ISBN)
-)
+
